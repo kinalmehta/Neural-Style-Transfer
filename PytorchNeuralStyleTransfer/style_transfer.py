a112a230db1934522b64f3df08ca9e1566a30e67
Kinal Mehta <kgm1995@gmail.com>
2020-03-09 15:39:15 +0530

    Neural Style Transfer combining and modifying 2 of the most awesome libraries

--- PytorchNeuralStyleTransfer/style_transfer.py
+++ PytorchNeuralStyleTransfer/style_transfer.py
@@ -19,30 +19,6 @@ from PIL import Image
 import cv2
 
 
-def get_vgg():
-    layers_list = [nn.Conv2d(3, 64, kernel_size=3, padding=1),
-                    nn.Conv2d(64, 64, kernel_size=3, padding=1),
-                    nn.MaxPool2d(kernel_size=2, stride=2),
-                    nn.Conv2d(64, 128, kernel_size=3, padding=1),
-                    nn.Conv2d(128, 128, kernel_size=3, padding=1),
-                    nn.MaxPool2d(kernel_size=2, stride=2),
-                    nn.Conv2d(128, 256, kernel_size=3, padding=1),
-                    nn.Conv2d(256, 256, kernel_size=3, padding=1),
-                    nn.Conv2d(256, 256, kernel_size=3, padding=1),
-                    nn.Conv2d(256, 256, kernel_size=3, padding=1),
-                    nn.MaxPool2d(kernel_size=2, stride=2),
-                    nn.Conv2d(256, 512, kernel_size=3, padding=1),
-                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                    nn.MaxPool2d(kernel_size=2, stride=2),
-                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                    nn.Conv2d(512, 512, kernel_size=3, padding=1),
-                    nn.MaxPool2d(kernel_size=2, stride=2)]
-    return nn.Sequential(*layers_list)
-
 #vgg definition that conveniently let's you grab the outputs from any layer
 class VGG(nn.Module):
     def __init__(self, pool='max'):
